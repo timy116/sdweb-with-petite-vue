3cbdd400852a236265de7c02f4cd8efb4efe53ce
TimyLian <timy117@gmail.com>
2024-05-28 18:10:54 +0800

    Update footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const info: string[] = [
-  '100212 臺北市中正區南海路 37 號',
+  '地址：100212 臺北市中正區南海路 37 號',
   '電話：+886-2-2381-2991',
   '傳真：886-2-2331-0341'
 ]
@@ -13,13 +13,18 @@ const copyright = 'Copyright © 農業部 版權所有'
   <footer>
     <div class="container">
       <div class="d-flex flex-column flex-lg-row justify-content-lg-between">
-        <div class="content d-flex py-2">
+        <!-- content -->
+        <div class="content d-flex flex-column py-2 flex-lg-column justify-content-xl-center">
           <ul
-            class="contact-info contact-info list-group list-group-flush d-flex flex-column flex-sm-row flex-wrap align-content-center">
+            class="contact-info list-group list-group-flush d-flex flex-column flex-sm-row flex-wrap">
             <li v-for="i in info" class="list-group-item">{{ i }}</li>
+          </ul>
+          <ul class="contact-info list-group list-group-flush d-flex flex-lg-column">
+            <li class="list-group-item">建議使用 Chrome，最佳瀏覽解析度為 1024x768 以上</li>
           </ul>
         </div>
 
+        <!-- copyright -->
         <div class="copyright text-center fw-bold my-2">
           <ul class="list-group list-group-flush d-flex flex-column">
             <li class="list-group-item">
